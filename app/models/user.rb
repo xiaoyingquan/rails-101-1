a9a9051cc1930306241779e7bae8ef8ed28a4a04
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-03-29 13:05:17 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :groups
   has_many :posts
+  has_many :group_relationships
+  hes_many :participatrd_groups, :through => :group_relationships, :source => :group
 end
